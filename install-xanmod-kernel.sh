7a8c58ee6590a1171ccb677ce33aa120bc853719
nescafe-gold <aidanlane02@protonmail.com>
2022-08-01 02:33:06 +0100

    fixed script

--- install-xanmod-kernel.sh
+++ install-xanmod-kernel.sh
@@ -2,6 +2,7 @@
 #set -e
 ##################################################################################################################
 # Author 	: Erik Dubois
+# Edited by Nescafe Gold
 # Website   : https://www.erikdubois.be
 # Website	: https://www.arcolinux.info
 # Website	: https://www.arcolinux.com
@@ -23,12 +24,14 @@ echo "################################################################"
 tput sgr0
 echo
 
+username=$(whoami)
+
 #https://wiki.archlinux.org/title/Modprobed-db
 
-sudo pacman -S modprobed-db --noconfirm --needed
-modprobed-db
-modprobed-db store
-systemctl --user enable --now modprobed-db.service
+su -c "paru -S modprobed-db --noconfirm --needed" $username
+su -c "modprobed-db" $username
+su -c "modprobed-db store" $username
+su -c "systemctl --user enable --now modprobed-db.service" $username
 
 echo
 tput setaf 2
@@ -38,70 +41,48 @@ echo "################################################################"
 tput sgr0
 echo
 
-
-#!/bin/bash
-#set -e
-##################################################################################################################
-# Author 	: Erik Dubois
-# Website   : https://www.erikdubois.be
-# Website	: https://www.arcolinux.info
-# Website	: https://www.arcolinux.com
-# Website	: https://www.arcolinuxd.com
-# Website	: https://www.arcolinuxb.com
-# Website	: https://www.arcolinuxiso.com
-# Website	: https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
-
 echo
 tput setaf 2
 echo "################################################################"
-echo "################### Start - Skylake cpu"
+echo "################### Start - Detecting CPU && No Multiple CPU's"
 echo "################################################################"
 tput sgr0
 echo
 
 #https://wiki.archlinux.org/title/Modprobed-db
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/linux-xanmod-tt.tar.gz
-
-tar xzf linux-xanmod-tt*
-
-rm linux-xanmod-tt.tar.gz
+git clone https://aur.archlinux.org/linux-xanmod-tt.git linux-xanmod-tt
+cd linux-xanmod-tt
 
 FIND="_microarchitecture=0"
 REPLACE="_microarchitecture=98"
-sed -i "s/$FIND/$REPLACE/g" linux-xanmod-tt/PKGBUILD
+sed -i "s/$FIND/$REPLACE/g" PKGBUILD
 
 # FIND="use_numa=y"
 # REPLACE="use_numa=n"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod-tt/PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
 
 # FIND="use_tracers=y"
 # REPLACE="use_tracers=n"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod-tt/PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
 
 # FIND="_compiler=gcc"
 # REPLACE="_compiler=gcc"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod-tt/PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
 
 # FIND="_compress_modules=n"
 # REPLACE="_compress_modules=n"
-# sed -i "s/$FIND/$REPLACE/g" linux-xanmod-tt/PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
 
 FIND="_localmodcfg=n"
 REPLACE="_localmodcfg=y"
-sed -i "s/$FIND/$REPLACE/g" linux-xanmod-tt-tt/PKGBUILD
+sed -i "s/$FIND/$REPLACE/g" PKGBUILD
 
-cd linux-xanmod-tt
+su -c "makepkg -s --skippgpcheck" $(whoami)
 
-makepkg -s --skippgpcheck
 
-cd $HOME 
-rm -r $HOME/linux-xanmod-tt
+cd ..
+rm -rf linux-xanmod-tt
 
 echo
 tput setaf 2
