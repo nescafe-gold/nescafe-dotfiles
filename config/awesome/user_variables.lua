a5833f2587cb0dfe94e50d246671a83959efd177
nescafe-gold <aidanlane02@protonmail.com>
2022-08-04 00:46:50 +0100

    iufg

--- config/awesome/user_variables.lua
+++ config/awesome/user_variables.lua
@@ -2,11 +2,11 @@ return {
   widget = {
     weather = {
       --- API Key
-      key = "f72bc2f9407781cb4d3468ed68d35528",
+      key = "",
       --- Coordinates
       coordinates = {
-        "52.5862", --- lat
-        "1.9829", --- lon
+        "", --- lat
+        "", --- lon
       },
     },
   },
