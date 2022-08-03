90823f9e02b100acdc4f28aaa77182bd4fee0edc
nescafe-gold <aidanlane02@protonmail.com>
2022-08-03 16:27:45 +0100

    add

--- install-xanmod-kernel.sh
+++ install-xanmod-kernel.sh
@@ -16,6 +16,21 @@
 #
 ##################################################################################################################
 
+# Pretty print (function).
+info_print () {
+    echo -e "${BOLD}${BGREEN}[ ${BYELLOW}•${BGREEN} ] $1${RESET}"
+}
+
+# Pretty print for input (function).
+input_print () {
+    echo -ne "${BOLD}${BYELLOW}[ ${BGREEN}•${BYELLOW} ] $1${RESET}"
+}
+
+# Alert user of bad input (function).
+error_print () {
+    echo -e "${BOLD}${BRED}[ ${BBLUE}•${BRED} ] $1${RESET}"
+}
+
 echo
 tput setaf 2
 echo "################################################################"
@@ -28,10 +43,17 @@ username=$(whoami)
 
 #https://wiki.archlinux.org/title/Modprobed-db
 
-su -c "paru -S modprobed-db --noconfirm --needed" $username
+package="modprobed-db"
+if paru -Qs $package > /dev/null ; then
+    continue
+else
+    su -c "paru -S modprobed-db --noconfirm --needed" $username
+fi
+
 su -c "modprobed-db" $username
 su -c "modprobed-db store" $username
 su -c "systemctl --user enable --now modprobed-db.service" $username
+su -c "sudo modprobed-db rebuild " $username
 
 echo
 tput setaf 2
@@ -51,38 +73,38 @@ echo
 
 #https://wiki.archlinux.org/title/Modprobed-db
 
-git clone https://aur.archlinux.org/linux-xanmod-tt.git linux-xanmod-tt
-cd linux-xanmod-tt
+git clone https://aur.archlinux.org/linux-xanmod-tt.git $HOME/linux-xanmod-tt
+cd $HOME/linux-xanmod-tt
 
-# FIND="_microarchitecture=0"
-# REPLACE="_microarchitecture=98"
-# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
+FIND="_microarchitecture=0"
+REPLACE="_microarchitecture=98"
+sed -i "s/$FIND/$REPLACE/g" $HOME/linux-xanmod-tt/PKGBUILD
 
 # FIND="use_numa=y"
 # REPLACE="use_numa=n"
-# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" $HOME/linux-xanmod-tt/PKGBUILD
 
 # FIND="use_tracers=y"
 # REPLACE="use_tracers=n"
-# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" $HOME/linux-xanmod-tt/PKGBUILD
 
 # FIND="_compiler=gcc"
 # REPLACE="_compiler=gcc"
-# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" $HOME/linux-xanmod-tt/PKGBUILD
 
 # FIND="_compress_modules=n"
 # REPLACE="_compress_modules=n"
-# sed -i "s/$FIND/$REPLACE/g" PKGBUILD
+# sed -i "s/$FIND/$REPLACE/g" $HOME/linux-xanmod-tt/PKGBUILD
 
 FIND="_localmodcfg=n"
 REPLACE="_localmodcfg=y"
-sed -i "s/$FIND/$REPLACE/g" PKGBUILD
+sed -i "s/$FIND/$REPLACE/g" $HOME/linux-xanmod-tt/PKGBUILD
 
-su -c "makepkg -s --skippgpcheck" $(whoami)
+su -c "makepkg -s --skippgpcheck -f" $(whoami)
 
 
-cd ..
-rm -rf linux-xanmod-tt
+cd $HOME
+rm -rf $HOME/linux-xanmod-tt
 
 echo
 tput setaf 2
