977e9876e4a83947dbbe61c49c2b4b904ecaff35
nescafe-gold <aidanlane02@protonmail.com>
2022-08-04 01:04:30 +0100

    add

--- config/zsh/cfg_parts/env.zsh
+++ config/zsh/cfg_parts/env.zsh
@@ -24,6 +24,7 @@ export XDG_VIDEOS_DIR="$HOME/Videos"
 export GEM_PATH="$XDG_DATA_HOME/ruby/gems"
 export GEM_SPEC_CACHE="$XDG_DATA_HOME/ruby/specs"
 export GEM_HOME="$XDG_DATA_HOME/ruby/gems"
+export PATH="$PATH:$GEM_HOME/bin"
 
 #Go
 export GOPATH="$XDG_DATA_HOME/go"
